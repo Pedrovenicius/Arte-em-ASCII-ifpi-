8a22b5f57e955734f82c155d90ff89fa0445509c
Pedrovenicius <112330605+Pedrovenicius@users.noreply.github.com>
2022-08-28 11:40:52 -0300

    Add files via upload

---
+++ oi.py
@@ -0,0 +1,42 @@
+print("oi, tudo bem ?")
+print ('''
+eu gosto de gatos ...
+
+  -
+ (  \
+   )  )    ********        
+ (   ( . -        - .   A .- .A
+  \  \/              \/  ,   , \
+   \              ,,, ;    t   /,,,
+    \      \           .      . 
+     \       \  ,,,      ,--- ,
+      \       |     .
+        /  / /      | | |
+       / _ ,) )     |_,)) ''')
+print("ola" * 5)
+
+print (" ola, "  +  "tudo bem ?" )
+
+
+
+
+print ("parabens !!!!! " )
+print ('''
+Aqui vai um dinossauro !
+
+              __
+             / _)
+    .-^^^ - / /
+ __/         /
+<__ .|_|-|_|  
+
+
+E aqui vai um bolo ! hummmmmmmmmmmmmmm ''')
+print("i" * 9)
+print ("#" *10)
+print ("="* 10 )
+print ("#" *10)
+print ("="* 10 )
+print ("#" *10)
+
+
